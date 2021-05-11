822ee55a219ff9e2a118827e46be20e0a2403dd9
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2021-05-11 22:01:41 +0000

    update profile
    update a profile

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -134,6 +134,34 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res)
 })
 
+// Get currently logged in user details Profile => /api/v1/me
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id)
+
+    res.status(200).json({
+        success:true,
+        user
+    })
+})
+
+// Update / change password => /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    // check previous user password
+    const isMatched = await user.comparePassword(req.body.oldPassword)
+
+    if(!isMatched){
+        return next(new ErrorHandler('Old Password is incorrect', 400));
+    }
+
+    user.password = req.body.password;
+    await user.save();
+    sendToken(user, 200, res)
+
+})
+
+
 
 // Logout user => /api/v1/logout
 exports.logout = catchAsyncErrors(async (req, res, next) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {registerUser , loginUser, logout, forgotPassword, resetPassword} = require('../controllers/authController');
+const { isAuthenticatedUser } = require('../middlewares/auth')
+
+const {registerUser , loginUser, logout, forgotPassword, resetPassword ,getUserProfile, updatePassword} = require('../controllers/authController');
+
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -10,4 +13,8 @@ router.route('/password/reset/:token').post(resetPassword)
 
 router.route('/logout').get(logout);
 
-module.exports = router;
+
+router.route('/me').get(isAuthenticatedUser, getUserProfile)
+router.route('/password/update').put(isAuthenticatedUser, updatePassword)
+
+module.exports = router
