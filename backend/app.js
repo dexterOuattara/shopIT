561a3453d6a4e75d3dd1153d5b70ad4dc2b3f641
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2021-06-05 21:15:50 +0000

    add x-powered-by
    x-powered-by

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app1 = express();
-
+app1.disable("x-powered-by");
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
